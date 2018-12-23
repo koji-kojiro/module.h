3c99569150c5938461390e22875133312ce1a75e
TANI Kojiro <kojiro0531@gmail.com>
2018-12-23 16:40:53 +0900

    Indent

--- module.h
+++ module.h
@@ -2,7 +2,7 @@
 
 #define constructor __attribute__ ((constructor)) void
 
-#define module(name)                                                          \
-  constructor __init__##name (void);                                          \
-  struct module_##name name;                                                  \
+#define module(name)                 \
+  constructor __init_##name (void);  \
+  struct module_##name name;         \
   struct module_##name
